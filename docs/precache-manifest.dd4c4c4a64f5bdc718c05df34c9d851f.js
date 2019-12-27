fe6e3359306f89ffe3f6028ee5e5dac058fe2b72
Liz Kalter <43416298+LizKalter@users.noreply.github.com>
2019-12-26 21:59:39 -0500

    updating with latest

---
+++ docs/precache-manifest.dd4c4c4a64f5bdc718c05df34c9d851f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26a3bd04959fbc3a139dbb0bde0c5b55",
+    "url": "/fcc-random-quote/index.html"
+  },
+  {
+    "revision": "7062e0932c99ae1a512d",
+    "url": "/fcc-random-quote/static/css/main.70e2d1da.chunk.css"
+  },
+  {
+    "revision": "4cdbc1278efc1774bdbd",
+    "url": "/fcc-random-quote/static/js/2.995593bc.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/fcc-random-quote/static/js/2.995593bc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7062e0932c99ae1a512d",
+    "url": "/fcc-random-quote/static/js/main.22f4fd23.chunk.js"
+  },
+  {
+    "revision": "a6ef89bba094ef07d536",
+    "url": "/fcc-random-quote/static/js/runtime-main.0608c199.js"
+  },
+  {
+    "revision": "a7a925c7c07f163f3274df0c1ab2c321",
+    "url": "/fcc-random-quote/static/media/Twitter_Logo_Blue.a7a925c7.svg"
+  }
+]);
--- src/index.js
+++ src/index.js
@@ -1,12 +1,120 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const quotes = [
+	{
+		text: "But I was going into Tosche Station to pick up some power converters!",
+		author: "Luke Skywalker"
+	},
+	{
+		text: "I find your lack of faith disturbing.",
+		author: "Darth Vader"
+	},
+	{
+		text: "You don’t need to see his identification… These aren’t the droids you’re looking for.",
+		author: "Obi-Wan Kenobi"
+	},
+	{
+		text: "Cover me, Porkins!",
+		author: "Biggs Darklighter"
+	},
+	{
+		text: "An elegant weapon for a more civilized age.",
+		author: "Obi-Wan Kenobi"
+	},
+	{
+		text: "Who’s the more foolish? The fool or the fool who follows him?",
+		author: "Obi-Wan Kenobi"
+	},
+	{
+		text: "Aren’t you a little short for a stormtrooper?",
+		author: "Leia Organa"
+	},
+	{
+		text: "Never tell me the odds.",
+		author: "Han Solo"
+	},
+	{
+		text: "We seem to be made to suffer. It’s our lot in life.",
+		author: "C-3PO"
+	},
+	{
+		text: "Do. Or do not. There is no try.",
+		author: "Yoda"
+	}
+];
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+class Quote extends React.Component {
+	render() {
+		return (
+			<div className="box">
+				<p id="text">{this.props.quote.text}</p>
+				<p id="author">{this.props.quote.author}</p>
+			</div>
+		);
+	}
+}
+
+class QuoteButton extends React.Component {
+	render() {
+		return (
+			<button id="new-quote" onClick={this.props.getQuote}>{'A New Quote'}</button>
+		);
+	}
+}
+
+class TweetButton extends React.Component {
+	constructor(props) {
+		super(props);
+		this.encodedQuote = this.encodedQuote.bind(this);
+	}
+
+	encodedQuote() {
+		return encodeURIComponent('"' + this.props.quote.text + '" ' + this.props.quote.author);
+	}
+	render() {
+		return (
+			<a href={'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text='+this.encodedQuote()} id="tweet-quote" aria-label="Tweet this quote">
+				<i></i>
+			</a>
+		);
+	}
+}
+
+class QuoteBox extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			currentQuote: this.getRandomQuote()
+		};
+		this.setRandomQuote = this.setRandomQuote.bind(this);
+	}
+
+	getRandomQuote() {
+		return quotes[Math.floor(Math.random() * (quotes.length-1))];
+	}
+
+	setRandomQuote() {
+		this.setState({
+			currentQuote: this.getRandomQuote()
+		});
+	}
+
+	render () {
+		return (
+			<div id="quote-box">
+				<Quote quote={this.state.currentQuote} />
+				<div className="buttons">
+					<QuoteButton getQuote={this.setRandomQuote} />
+					<TweetButton quote={this.state.currentQuote}/>
+				</div>
+			</div>
+		);
+	}
+}
+
+ReactDOM.render(<QuoteBox />, document.getElementById('root'));
+
 serviceWorker.unregister();
